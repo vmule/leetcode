cc0deb52a60d1ecdb1f82ec786e673416794cb0e
Vito Mule <mulevito@gmail.com>
2023-09-03 22:16:02 -0700

    121.maxProfit

---
+++ python/max_profit.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    """
+    https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+    """
+
+    def maxProfit(self, prices: List[int]) -> int:
+        lowest_price = prices[0]
+        highest_profit = 0
+
+        for price in prices[1:]:
+            if (price - lowest_price) > highest_profit:
+                highest_profit = price - lowest_price
+            if price < lowest_price:
+                lowest_price = price
+
+        return highest_profit
