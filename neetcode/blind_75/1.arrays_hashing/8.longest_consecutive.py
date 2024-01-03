655f847d081d2661360acd28a300c5813a22bbe1
Vito Mule <mulevito@gmail.com>
2024-01-02 20:34:32 -0800

    128 cleanup

--- neetcode/blind_75/1.arrays_hashing/8.longest_consecutive.py
+++ neetcode/blind_75/1.arrays_hashing/8.longest_consecutive.py
@@ -7,20 +7,13 @@ class Solution:
     """
 
     def longestConsecutive(self, nums: List[int]) -> int:
-        if len(nums) < 1:
-            return 0
-
-        length = 1
+        longest = 0
         set_nums = set(nums)
 
         for n in nums:
-            if n - 1 in set_nums:
-                pass
-            else:
-                tmp = n
-                tmp_length = 1
-                while (tmp + 1) in set_nums:
-                    tmp_length += 1
-                    tmp += 1
-                length = max(length, tmp_length)
-        return length
+            if n - 1 not in set_nums:
+                length = 1
+                while (n + length) in set_nums:
+                    length += 1
+                longest = max(longest, length)
+        return longest
