5c2a77f69850331ae11de4007385fb7bf5bb6d77
Vito Mule <mulevito@gmail.com>
2024-01-05 09:01:00 -0800

    347.topKFrequent

--- python/top_k_frequent.py
+++ python/top_k_frequent.py
@@ -9,26 +9,18 @@ class Solution:
     """
 
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # Alternative solutions, fails for very large list
+        count = {}
+        freq = [[] for _ in range(len(nums) + 1)]
 
-        # nums_set: Set[int] = set(nums)
-        # output: List[int] = []
-        #
-        # for _ in range(k):
-        #     output.append(max(nums_set, key=nums.count))
-        #     nums_set.remove(output[-1])
-        # return output
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for num, n_count in count.items():
+            freq[n_count].append(num)
 
-        my_dict: Dict[int, int] = {}
-        output: List[int] = []
+        output = []
 
-        for element in nums:
-            if element in my_dict:
-                my_dict[element] += 1
-            else:
-                my_dict[element] = 1
-        for _ in range(k):
-            champion = max(my_dict, key=my_dict.__getitem__)
-            output.append(champion)
-            del my_dict[champion]
-        return output
+        for value in freq[::-1]:
+            for e in value:
+                output.append(e)
+                if len(output) == k:
+                    return output
