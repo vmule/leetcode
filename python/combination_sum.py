ccef797bb1c90875e147e1dc30e148c3928250ec
Vito Mule <mulevito@gmail.com>
2024-01-06 19:25:35 -0800

    39.combinationSum

---
+++ python/combination_sum.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    """
+    https://leetcode.com/problems/combination-sum
+    https://www.youtube.com/watch?v=GBKI9VSKdGg
+    """
+
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def dfs(i, cur, total):
+            if total == target:
+                res.append(cur.copy())
+                return
+            if i >= len(candidates) or total > target:
+                return
+
+            cur.append(candidates[i])
+            dfs(i, cur, total + candidates[i])
+            cur.pop()
+            dfs(i + 1, cur, total)
+
+        dfs(0, [], 0)
+        return res
