9a439817b72fb309655c820492c4020a56887e4f
Vito Mule <mulevito@gmail.com>
2024-01-07 15:02:51 -0800

    79.wordSearch

---
+++ python/word.search.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows = len(board)
+        cols = len(board[0])
+
+        seen_elements = set()
+
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if (
+                r < 0
+                or c < 0
+                or r >= rows
+                or c >= cols
+                or word[i] != board[r][c]
+                or (r, c) in seen_elements
+            ):
+                return False
+
+            seen_elements.add((r, c))
+            res = (
+                dfs(r + 1, c, i + 1)
+                or dfs(r - 1, c, i + 1)
+                or dfs(r, c + 1, i + 1)
+                or dfs(r, c - 1, i + 1)
+            )
+            seen_elements.remove((r, c))
+            return res
+
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0):
+                    return True
+        return False
