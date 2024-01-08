44b293e94185f9e5a9c85d4f8e3cf9c3e0e7ad84
Vito Mule <mulevito@gmail.com>
2024-01-08 15:40:41 -0800

    121.maxProfit

--- python/max_profit.py
+++ python/max_profit.py
@@ -7,13 +7,27 @@ class Solution:
     """
 
     def maxProfit(self, prices: List[int]) -> int:
-        lowest_price = prices[0]
-        highest_profit = 0
+        l, r = 0, 1
+        profit = 0
+        max_profit = 0
 
-        for price in prices:
-            if (price - lowest_price) > highest_profit:
-                highest_profit = price - lowest_price
-            if price < lowest_price:
-                lowest_price = price
+        while r < len(prices):
+            if prices[r] > prices[l]:
+                profit = prices[r] - prices[l]
+                max_profit = max(max_profit, profit)
+            else:
+                l = r
+            r += 1
+        return max_profit
 
-        return highest_profit
+        #    def maxProfit(self, prices: List[int]) -> int:
+        #        lowest_price = prices[0]
+        #        highest_profit = 0
+        #
+        #        for price in prices:
+        #            if (price - lowest_price) > highest_profit:
+        #                highest_profit = price - lowest_price
+        #            if price < lowest_price:
+        #                lowest_price = price
+        #
+        #        return highest_profit
