040516ef25b973285807cd1c3fe99c0eb4a7c407
Vito Mule <mulevito@gmail.com>
2024-01-11 22:11:06 -0800

    383.canConstruct

--- python/can_construct.py
+++ python/can_construct.py
@@ -7,4 +7,15 @@ class Solution:
     """
 
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        return Counter(ransomNote) - Counter(magazine) == {}
+        # return Counter(ransomNote) - Counter(magazine) == {}
+
+        m_count = {}
+        for char in magazine:
+            m_count[char] = m_count.get(char, 0) + 1
+
+        for char in ransomNote:
+            if m_count.get(char, 0) > 0:
+                m_count[char] -= 1
+            else:
+                return False
+        return True
