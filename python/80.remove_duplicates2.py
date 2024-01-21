fd10fd87a8c05817e936bc502eb6f8bedb2239e8
Vito Mule <mulevito@gmail.com>
2024-01-21 13:54:46 -0800

    80.removeDuplicates

--- python/80.remove_duplicates2.py
+++ python/80.remove_duplicates2.py
@@ -6,11 +6,16 @@ class Solution:
     https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii
     """
 
-    def removeDuplicates2(self, nums: List[int]) -> int:
-        i = 2
-        while i < len(nums):
-            if nums[i] == nums[i - 2]:
-                nums.pop(i)
-            else:
-                i = i + 1
-        return i
+    def removeDuplicates(self, nums: List[int]) -> int:
+        l, r = 0, 0
+
+        while r < len(nums):
+            count = 1
+            while r + 1 < len(nums) and nums[r] == nums[r + 1]:
+                r += 1
+                count += 1
+            for _ in range(min(2, count)):
+                nums[l] = nums[r]
+                l += 1
+            r += 1
+        return l
