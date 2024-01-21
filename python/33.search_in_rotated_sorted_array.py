181bff0f21a49868956af40c82552cdc192c30e1
Vito Mule <mulevito@gmail.com>
2024-01-21 13:21:12 -0800

    33.search

--- python/33.search_in_rotated_sorted_array.py
+++ python/33.search_in_rotated_sorted_array.py
@@ -7,23 +7,33 @@ class Solution:
     """
 
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
+        left, right = 0, len(nums) - 1
 
-        while l <= r:
-            m = (l + r) // 2
-            if nums[m] == target:
-                return m
-            # left portion
-            if nums[m] >= nums[l]:
-                if target > nums[m] or target < nums[l]:
-                    l = m + 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            # if the value at mid is greather or equal then the most left value
+            # we are in the left sorted portion of the array left portion
+            # target = 0
+            # [4,5,6,7,0,1,2]
+            # middle would be 7
+            # 7 >= 4
+            if nums[mid] >= nums[left]:
+                # [4,5,6,7] target = 0, l = 4, mid = 7
+                # if target is greater than the value at index mid
+                # or the smaller than the value at index left
+                # it means our result is in the part of the array between mid+1 and right
+                if target > nums[mid] or target < nums[left]:
+                    left = mid + 1
+                # else it means our result is in the array between left and mid-1
                 else:
-                    r = m - 1
-            # right portion
+                    right = mid - 1
+            # if not we are in the array sorted right portion
             else:
-                if target < nums[m] or target > nums[r]:
-                    r = m - 1
+                if target < nums[mid] or target > nums[right]:
+                    right = mid - 1
                 else:
-                    l = m + 1
+                    left = mid + 1
 
         return -1
