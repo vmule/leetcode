25c01ab0f886ef83808e4a06d1015e7869ca8c1a
Vito Mule <mulevito@gmail.com>
2024-01-23 21:02:28 -0800

    36.isValidSudoku

---
+++ python/36.valid_sudoku.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)  # key = (r // 3, c // 3)
+
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if board[r][c] == ".":
+                    continue
+                if (
+                    board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]
+                ):
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+        return True
