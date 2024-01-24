bcee1f4b09b48b023b86333e27c524893209cfc7
Vito Mule <mulevito@gmail.com>
2024-01-23 21:13:51 -0800

    704.binarySearch

---
+++ python/704.binary_search.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        if len(nums) == 1 and nums[0] == target:
+            return 0
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+        return -1
