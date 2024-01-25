40675611da8f3969773f824be3ca7c84149ba884
Vito Mule <mulevito@gmail.com>
2024-01-24 21:40:51 -0800

    Cleanup

--- python/121.max_profit.py
+++ python/121.max_profit.py
@@ -19,15 +19,3 @@ class Solution:
                 l = r
             r += 1
         return max_profit
-
-        #    def maxProfit(self, prices: List[int]) -> int:
-        #        lowest_price = prices[0]
-        #        highest_profit = 0
-        #
-        #        for price in prices:
-        #            if (price - lowest_price) > highest_profit:
-        #                highest_profit = price - lowest_price
-        #            if price < lowest_price:
-        #                lowest_price = price
-        #
-        #        return highest_profit
