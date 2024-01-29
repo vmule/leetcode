8a6e1463a00597910c7c0824cbffab0c6650c568
Vito Mule <mulevito@gmail.com>
2024-01-28 21:04:58 -0800

    55.jump

--- python/55.jump_game.py
+++ python/55.jump_game.py
@@ -6,7 +6,8 @@ class Solution:
         goal = len(nums) - 1
 
         for i in range(len(nums) - 1, -1, -1):
-            if i + nums[i] >= goal:
+            # if i + nums[i] >= goal:
+            if nums[i] >= goal - i:
                 goal = i
         if goal == 0:
             return True
