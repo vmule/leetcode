ecd6d6ffd19c75917060c3001fa05c518d8ffb05
Vito Mule <mulevito@gmail.com>
2024-01-28 20:22:53 -0800

    232.myQueue

---
+++ python/232.implement_queue_using_stacks.py
@@ -0,0 +1,32 @@
+class MyQueue:
+    def __init__(self):
+        self.stack_1 = []
+        self.stack_2 = []
+
+    def push(self, x: int) -> None:
+        self.stack_1.append(x)
+
+    def pop(self) -> int:
+        if len(self.stack_2) == 0:
+            while len(self.stack_1) > 0:
+                element = self.stack_1.pop()
+                self.stack_2.append(element)
+        return self.stack_2.pop()
+
+    def peek(self) -> int:
+        if len(self.stack_2) == 0:
+            while len(self.stack_1) > 0:
+                element = self.stack_1.pop()
+                self.stack_2.append(element)
+        return self.stack_2[-1]
+
+    def empty(self) -> bool:
+        return (len(self.stack_1) + len(self.stack_2)) == 0
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
