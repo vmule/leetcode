cf67108d8d3c784cbb379d44864fe075278b803d
Vito Mule <mulevito@gmail.com>
2024-01-31 02:29:15 -0800

    various/find_duplicate_files, add multiprocess

--- python/various/find_duplicate_files.py
+++ python/various/find_duplicate_files.py
@@ -5,16 +5,15 @@ import time
 import unittest
 from collections import defaultdict
 from hashlib import md5 as hash
-from multiprocessing.dummy import Pool as ThreadPool
+from multiprocessing import Pool
 
 
 # read chunks for files larger than 100 MB
 FILE_MAX_SIZE = 104857600
-MAX_THREAD = os.cpu_count()
-HASHES_DICT = defaultdict(list)
+MAX_P_PROCESS = os.cpu_count()
 
 
-def find_hashes(file, files_hash=HASHES_DICT):
+def find_hashes(file):
     with open(file, "rb") as f:
         if os.stat(file).st_size >= FILE_MAX_SIZE:
             file_hash = hash()
@@ -25,11 +24,13 @@ def find_hashes(file, files_hash=HASHES_DICT):
         else:
             file_hash = hash(f.read())
     file_hash_digest = file_hash.hexdigest()
-    files_hash[file_hash_digest].append(file)
+    return (file_hash_digest, file)
 
 
-def find_duplicate_files(path, multithread=True):
+def find_duplicate_files(path, multiprocess=True):
+
     files_size = defaultdict(list)
+    hashes_dict = defaultdict(list)
 
     for root, dirs, files in os.walk(path):
         for file in files:
@@ -45,94 +46,104 @@ def find_duplicate_files(path, multithread=True):
             for file in bucket:
                 duplicates_candidates.append(file)
 
-    if multithread is True:
-        pool = ThreadPool(MAX_THREAD)
-        pool.map(find_hashes, duplicates_candidates)
+    if multiprocess is True:
+        pool = Pool(MAX_P_PROCESS)
+        results = pool.map(find_hashes, duplicates_candidates)
+        for element in results:
+            key, file = element
+            hashes_dict[key].append(file)
     else:
         for file in duplicates_candidates:
-            find_hashes(file, HASHES_DICT)
+            key, file = find_hashes(file)
+            hashes_dict[key].append(file)
 
     results = []
-    for value in HASHES_DICT.values():
+    for value in hashes_dict.values():
         if len(value) > 1:
             results.append(value)
 
     return sorted(results)
 
 
-if not os.path.isdir("/tmp/a"):
-    os.mkdir("/tmp/a")
-if not os.path.isdir("/tmp/a/b"):
-    os.mkdir("/tmp/a/b")
-if not os.path.isdir("/tmp/a/b/c"):
-    os.mkdir("/tmp/a/b/c")
+def main():
+    if not os.path.isdir("/tmp/a"):
+        os.mkdir("/tmp/a")
+    if not os.path.isdir("/tmp/a/b"):
+        os.mkdir("/tmp/a/b")
+    if not os.path.isdir("/tmp/a/b/c"):
+        os.mkdir("/tmp/a/b/c")
 
-# unique files
-with open("/tmp/a/file11", "w") as f:
-    f.write("Text 00000000000")
-with open("/tmp/a/file12", "w") as f:
-    f.write("Text 000000000000")
-with open("/tmp/a/file13", "w") as f:
-    f.write("Text 0000000000000")
-with open("/tmp/a/file14", "w") as f:
-    f.write("Text 00000000000000")
-with open("/tmp/a/file15", "w") as f:
-    f.write("Text 000000000000000")
-with open("/tmp/a/file16", "w") as f:
-    f.write("Text 0000000000000000")
+    # unique files
+    with open("/tmp/a/file11", "w") as f:
+        f.write("Text 00000000000")
+    with open("/tmp/a/file12", "w") as f:
+        f.write("Text 000000000000")
+    with open("/tmp/a/file13", "w") as f:
+        f.write("Text 0000000000000")
+    with open("/tmp/a/file14", "w") as f:
+        f.write("Text 00000000000000")
+    with open("/tmp/a/file15", "w") as f:
+        f.write("Text 000000000000000")
+    with open("/tmp/a/file16", "w") as f:
+        f.write("Text 0000000000000000")
 
-# duplicates
-with open("/tmp/a/file1", "w") as f:
-    f.write("Text one")
-with open("/tmp/a/b/c/file2", "w") as f:
-    f.write("Text one")
-with open("/tmp/a/b/file3", "w") as f:
-    f.write("Text three")
-with open("/tmp/a/file4", "w") as f:
-    f.write("Text three")
+    # duplicates
+    with open("/tmp/a/file1", "w") as f:
+        f.write("Text one")
+    with open("/tmp/a/b/c/file2", "w") as f:
+        f.write("Text one")
+    with open("/tmp/a/b/file3", "w") as f:
+        f.write("Text three")
+    with open("/tmp/a/file4", "w") as f:
+        f.write("Text three")
+
+    path = "/tmp/a"
+
+    expected_result = [
+        ["/tmp/a/file1", "/tmp/a/b/c/file2"],
+        ["/tmp/a/file4", "/tmp/a/b/file3"],
+    ]
+    test = unittest.TestCase()
+
+    start_time = time.time()
+    duplicates = find_duplicate_files(path)
+    print(
+        "--- Multiprocess execution took  %s seconds ---" % (time.time() - start_time)
+    )
+
+    print(" ")
+
+    for element in duplicates:
+        print(element)
+
+    print(" ")
+
+    test.assertEqual(len(duplicates), len(expected_result))
+    for i in range(len(duplicates)):
+        test.assertCountEqual(duplicates[i], expected_result[i])
+    print(" ")
+
+    start_time = time.time()
+    duplicates = find_duplicate_files(path, multiprocess=False)
+    print(
+        "--- Singleprocess execution took %s seconds ---" % (time.time() - start_time)
+    )
+
+    print(" ")
+
+    for element in sorted(duplicates):
+        print(element)
+
+    print(" ")
+
+    test.assertEqual(len(duplicates), len(expected_result))
+    for i in range(len(duplicates)):
+        test.assertCountEqual(duplicates[i], expected_result[i])
+
+    print("All tests passed!!")
+
+    shutil.rmtree("/tmp/a")
 
 
-path = "/tmp/a"
-
-expected_result = [
-    ["/tmp/a/file1", "/tmp/a/b/c/file2"],
-    ["/tmp/a/file4", "/tmp/a/b/file3"],
-]
-test = unittest.TestCase()
-
-start_time = time.time()
-duplicates = find_duplicate_files(path)
-print("--- Multithread execution took  %s seconds ---" % (time.time() - start_time))
-
-print(" ")
-
-for element in duplicates:
-    print(element)
-
-print(" ")
-
-
-test.assertEqual(len(duplicates), len(expected_result))
-for i in range(len(duplicates)):
-    test.assertCountEqual(duplicates[i], expected_result[i])
-print(" ")
-
-HASHES_DICT.clear()
-start_time = time.time()
-duplicates = find_duplicate_files(path, multithread=False)
-print("--- Singlethread execution took %s seconds ---" % (time.time() - start_time))
-
-print(" ")
-
-for element in sorted(duplicates):
-    print(element)
-
-print(" ")
-
-test.assertEqual(len(duplicates), len(expected_result))
-for i in range(len(duplicates)):
-    test.assertCountEqual(duplicates[i], expected_result[i])
-
-print("All tests passed!!")
-
-shutil.rmtree("/tmp/a")
+if __name__ == "__main__":
+    main()
