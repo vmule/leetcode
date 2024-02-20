9a07efbb801b90b29a610b48b08383c0a87f9d60
Vito Mulè <mulevito@gmail.com>
2024-02-20 11:41:13 -0800

    268.missingNumber

--- python/268.missing_number.py
+++ python/268.missing_number.py
@@ -1,6 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        setN = set(nums)
-        for n in range(len(nums)+1):
-            if n not in setN:
-                return n
+        sum1 = 0
+        sum2 = 0
+        for i in range(len(nums)+1):
+            sum1 += i
+            if i < len(nums):
+                sum2 += nums[i]
+        return sum1 - sum2
