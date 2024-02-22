f5f14d690cd0f0739584b7931a9aa8502fbabf5b
Vito Mule <mulevito@gmail.com>
2024-02-22 00:28:05 -0800

    997.findJudge

--- python/997.find_the_town_judge.py
+++ python/997.find_the_town_judge.py
@@ -1,22 +1,17 @@
+from collections import defaultdict
 from typing import List
+
 
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
 
-        if len(trust) == n:
-            return -1
-        if n == 1 and len(trust) == 0: 
-            return 1
-        if n >= 1 and len(trust) == 0:
-            return -1
+        incoming = defaultdict(int)
+        outgoing = defaultdict(int)
+        for src, dst in trust:
+            incoming[dst] += 1
+            outgoing[src] += 1
 
-        trust_count = defaultdict(int)
-        trusters = defaultdict(int)
-        for e in trust:
-            trust_count[e[1]] += 1
-            trusters[e[0]] += 1
-
-        for key, value in trust_count.items():
-            if value == n-1 and key not in trusters:
-                return key
+        for i in range(1, n + 1):
+            if outgoing[i] == 0 and incoming[i] == n - 1:
+                return i
         return -1
