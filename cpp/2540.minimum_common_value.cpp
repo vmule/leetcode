eb50a7b1df61c3e20758daf36f422949dc91b3b4
Vito Mule <mulevito@gmail.com>
2024-03-13 00:41:08 -0700

    2540.getCommon.cpp

---
+++ cpp/2540.minimum_common_value.cpp
@@ -0,0 +1,23 @@
+#import <vector>
+
+class Solution {
+public:
+    int getCommon(std::vector<int>& nums1, std::vector<int>& nums2) {
+
+        int p1 = 0;
+        int p2 = 0;
+
+        while (p1 < nums1.size() and p2 < nums2.size()){
+            if (nums1[p1] == nums2[p2]){
+                return nums1[p1];
+            }
+            else if (nums1[p1] < nums2[p2]){
+                ++p1;
+            }
+            else{
+                ++p2;
+            }
+        }
+        return -1;
+    }
+};
