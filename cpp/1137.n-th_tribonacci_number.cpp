1edd13963697ec1b96b43e63e32641701cec3ba8
Vito Mulè <mulevito@gmail.com>
2024-04-23 20:19:29 -0700

    1137.n-th_tribonacci_number.cpp
    1137.n-th_tribonacci_number.cpp

---
+++ cpp/1137.n-th_tribonacci_number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+ public:
+    int tribonacci(int n) {
+        if (n < 1) {
+            return 0;
+        }
+
+        int fibs[] = {0, 0, 1};
+
+        --n;
+        while (n > 0) {
+            int fibs_n = fibs[0] + fibs[1] + fibs[2];
+            fibs[0] = fibs[1];
+            fibs[1] = fibs[2];
+            fibs[2] = fibs_n;
+            --n;
+        }
+        return fibs[2];
+    }
+};
